f0778567236d1e0e035aa4e8cfea5299ac558f25
RokeIturralde <rokeitumendi@gmail.com>
2022-12-16 08:09:03 +0100

    added the ApplicationConfig file

---
+++ src/java/service/ApplicationConfig.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/*
+ * @author JulenB
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /* Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(service.AlbumFacadeREST.class);
+        resources.add(service.ContentFacadeREST.class);
+        resources.add(service.CustomImageFacadeREST.class);
+        resources.add(service.CustomTextFacadeREST.class);
+        resources.add(service.MemberFacadeREST.class);
+        resources.add(service.MembershipPlanFacadeREST.class);
+        resources.add(service.UserFacadeREST.class);
+    }
+
+}
