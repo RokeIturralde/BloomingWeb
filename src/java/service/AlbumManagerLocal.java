a8867b057ea5578ce64579c30f3508d85059fcc0
2dam <2dam@TartangaLH.eus>
2022-12-21 14:16:40 +0100

    Avance favorable de la implementación de los metodos de la interfaz de Album

--- src/java/service/AlbumManagerLocal.java
+++ src/java/service/AlbumManagerLocal.java
@@ -7,72 +7,135 @@ package service;
 
 import entities.Album;
 import entities.User;
+import exceptions.*;
 import java.util.ArrayList;
 import java.util.Date;
 import javax.ejb.Local;
 
 /**
  * Local interface for AlbumManager EJB.
+ *
  * @author nerea
  */
 @Local
 public interface AlbumManagerLocal {
+
     /**
      * This method creates a new Album in the data base.
+     *
      * @param album The Album entity object containing new Album data.
-     * @throws
+     * @throws CreateException Thrown when any error or exception occurs during
+     * creation.
      */
-    public void createAlbum(Album album);
-     /**
-    * 
-    * @param album
-    * @param user
-    * @return 
-    * @throws
-    */
-    public ArrayList<Album> findMyAllAlbums(Album album, User user);
-    
+    public void createAlbum(Album album) throws CreateException;
+
     /**
-     * This method finds all the albums that the name contains
-     * the words the user introduce and the user is the creator
-     * @param album An Album entity object containing Album data
+     * This method updates a movement data in the data store.
+     *
+     * @param album The Album entity object containing modified Album data.
+     * @throws UpdateException Thrown when any error or exception occurs during
+     * update.
+     */
+    public void updateAlbum(Album album) throws UpdateException;
+
+    /**
+     * This method removes an account from the data store.
+     *
+     * @param album The Album entity object to be removed.
+     * @throws DeleteException Thrown when any error or exception occurs during
+     * deletion.
+     */
+    public void removeAlbum(Album album) throws DeleteException;
+
+    /**
+     * The method shares an existing album from the data store.
+     *
+     * @param album The Album entity object to be shared.
+     * @param login The user's login you want to share the album to
+     * @throws SharingException Thrown when any error or exception occurs during
+     * sharing.
+     */
+    public void shareAlbum(Album album, String login) throws SharingException;
+
+    /**
+     *
+     * @param album An Album entity object containing Album data.
      * @param user
-     * @param name
-     * @return 
-     * @throws
+     * @return
+     * @throws ReadException Thrown when any error or exception occurs during
+     * reading.
      */
-    public ArrayList<Album> findAlbumsByName(Album album, User user, String name);
-    
+    public ArrayList<Album> findMyAllAlbums(Album album, User user) throws ReadException;
+
     /**
-     * This method finds all the albums that the date 
-     * of creation is the date that the user introduce 
+     * This method finds all the albums that the name contains the words the
+     * user introduce and the user is the creator.
+     *
+     * @param album An Album entity object containing Album data.
+     * @param user A User entity object containing User data.
+     * @param name A String that contains the words the user introduced.
+     * @return An ArrayList of Albums that contains the albums that the method
+     * found.
+     * @throws ReadException Thrown when any error or exception occurs during
+     * reading.
+     */
+    public ArrayList<Album> findAlbumsByName(Album album, User user, String name) throws ReadException;
+
+    /**
+     * This method finds all the albums that the date of creation is the date
+     * that the user introduce
+     *
      * @param album An Album entity object containing Album data
      * @param user
      * @param date
-     * @return 
-     * @throws
+     * @return
+     * @throws ReadException Thrown when any error or exception occurs during
+     * reading.
      */
-    public ArrayList<Album> findAlbumByDate(Album album, User user, Date date);
-    
+    public ArrayList<Album> findAlbumsByDate(Album album, User user, Date date) throws ReadException;
+
     /**
-     * This method finds all the albums that the name contains
-     * the words the user introduce and someone shared to the user
-     * @param album
+     * This method finds all the albums that the name contains the words the
+     * user introduce and someone shared to the user
+     *
+     * @param album An Album entity object containing Album data
      * @param user
-     * @return 
-     * @throws
+     * @return
+     * @throws ReadException Thrown when any error or exception occurs during
+     * reading.
      */
-    public ArrayList<Album> findMyAllSharedAlbums(Album album, User user);
-    
+    public ArrayList<Album> findMyAllSharedAlbums(Album album, User user) throws ReadException;
+
     /**
-     * 
-     * @param album
+     *
+     * @param album An Album entity object containing Album data
      * @param user
      * @param name
-     * @return 
+     * @return
+     * @throws ReadException Thrown when any error or exception occurs during
+     * reading.
      */
-    public ArrayList<Album> findSharedAlbumName(Album album, User user, String name);
-    
-    public ArrayList<Album> findSharedAlbumDate(Album album, User user, Date date);
-    
+    public ArrayList<Album> findSharedAlbumsByName(Album album, User user, String name) throws ReadException;
+
+    /**
+     *
+     * @param album An Album entity object containing Album data
+     * @param user
+     * @param date
+     * @return
+     * @throws ReadException Thrown when any error or exception occurs during
+     * reading.
+     */
+    public ArrayList<Album> findSharedAlbumsByDate(Album album, User user, Date date) throws ReadException;
+
+    /**
+     *
+     * @param album An Album entity object containing Album data
+     * @param user
+     * @return
+     * @throws ReadException Thrown when any error or exception occurs during
+     * reading.
+     */
+    public ArrayList<Album> findSharedAlbumsByCreator(Album album, User user) throws ReadException;
+
 }
--- src/java/service/EJBAlbumManager.java
+++ src/java/service/EJBAlbumManager.java
@@ -6,9 +6,10 @@
 package service;
 
 import entities.Album;
+import entities.User;
+import exceptions.*;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -17,19 +18,130 @@ import javax.persistence.PersistenceContext;
 /**
  * This is the stateless EJB that implements the AlbumManagerLocal interface for
  * AlbumManager web service application.
+ *
  * @author nerea
  */
 @Stateless
-public class EJBAlbumManager implements AlbumManagerLocal{
+public class EJBAlbumManager implements AlbumManagerLocal {
+
     private static final Logger LOGGER = Logger.getLogger("package service");
     /**
      * EntityManager for DataModelExamplePU persistence unit.
      */
     @PersistenceContext(unitName = "DataModelExamplePU")
     private EntityManager em;
-    
 
-   /* public ArrayList<Album> myAlbums() {
+    /**
+     * This method creates a new Album in the data base.
+     *
+     * @param album The Album entity object containing new Album data.
+     * @throws CreateException Thrown when any error or exception occurs during
+     * creation.
+     */
+    @Override
+    public void createAlbum(Album album) throws CreateException {
+        try {
+            em.persist(album);
+        } catch (Exception e) {
+            throw new CreateException(e.getMessage());
+        }
+    }
+
+    /**
+     * This method updates a movement data in the data store.
+     *
+     * @param album The Album entity object containing modified Album data.
+     * @throws UpdateException Thrown when any error or exception occurs during
+     * update.
+     */
+    @Override
+    public void updateAlbum(Album album) throws UpdateException {
+        try {
+            if (!em.contains(album)) {
+                em.merge(album);
+            }
+            em.flush();
+        } catch (Exception e) {
+            throw new UpdateException(e.getMessage());
+        }
+    }
+
+    /**
+     * This method removes an account from the data store.
+     *
+     * @param album The Album entity object to be removed.
+     * @throws DeleteException Thrown when any error or exception occurs during
+     * deletion.
+     */
+    @Override
+    public void removeAlbum(Album album) throws DeleteException {
+        try {
+            em.remove(em.merge(album));
+        } catch (Exception e) {
+            throw new DeleteException(e.getMessage());
+        }
+    }
+    
+    /**
+     * The method shares an existing album from the data store.
+     *
+     * @param album The Album entity object to be shared.
+     * @param login The user's login you want to share the album to
+     * @throws SharingException Thrown when any error or exception occurs during
+     * sharing.
+     */
+    @Override
+    public void shareAlbum(Album album, String login) throws SharingException {
+        
+    }
+
+    @Override
+    public ArrayList<Album> findMyAllAlbums(Album album, User user) throws ReadException {
+        return null;
+    }
+    
+    /**
+     * This method finds all the albums that the name contains the words the
+     * user introduce and the user is the creator.
+     *
+     * @param album An Album entity object containing Album data.
+     * @param user A User entity object containing User data.
+     * @param name A String that contains the words the user introduced.
+     * @return An ArrayList of Albums that contains the albums that the method
+     * found.
+     * @throws ReadException
+     */
+    @Override
+    public ArrayList<Album> findAlbumsByName(Album album, User user, String name) throws ReadException {
+       return null;
+    }
+
+    @Override
+    public ArrayList<Album> findAlbumsByDate(Album album, User user, Date date) throws ReadException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Album> findMyAllSharedAlbums(Album album, User user) throws ReadException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Album> findSharedAlbumsByName(Album album, User user, String name) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Album> findSharedAlbumsByDate(Album album, User user, Date date) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Album> findSharedAlbumsByCreator(Album album, User user) throws ReadException {
+        return null;
+    } 
+    
+    /* public ArrayList<Album> myAlbums() {
         ArrayList<Album> myAlbums = null;
         try {
 
