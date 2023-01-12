05a9494f7b26c8c0a3cce458ddd2b91d47616632
optickx <danielbarrios2002@gmail.com>
2023-01-12 12:05:15 +0100

    ending the facade rest

--- src/java/service/UserFacadeREST.java
+++ src/java/service/UserFacadeREST.java
@@ -5,8 +5,12 @@
  */
 package service;
 
+import entities.Privilege;
+import entities.Status;
 import entities.User;
 import exceptions.CreateException;
+import exceptions.FindUserException;
+import exceptions.UpdateException;
 import service.user.IUserManager;
 
 import java.util.List;
@@ -25,6 +29,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.glassfish.security.services.common.PrivilededLookup;
 
 
 /**
@@ -54,29 +60,86 @@ public class UserFacadeREST {
     @PUT
     @Path("{id}")
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void edit(@PathParam("id") Integer id, User entity) {
-        
+    public void edit(@PathParam("login") String login, User entity) {
+        try {
+            ejb.updateUser(entity);
+        } catch (UpdateException ejb) {
+            throw new InternalServerErrorException(ejb.getMessage());
+        }
     }
 
     @DELETE
     @Path("{id}")
-    public void remove(@PathParam("id") Integer id) {
-        
+    public void remove(@PathParam("login") String login) {
+        try {
+            ejb.removeUser(login);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
 
     @GET
     @Path("{id}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public User find(@PathParam("id") Integer id) {
-        return null;
+    public User find(@PathParam("login") String login) {
+        try {
+            return ejb.findUserByLogin(login);
+        } catch (Exception e) {
+            throw new InternalServerErrorException();
+        }
+    }
+    /*
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<User> findAll() {
+        try {
+            ejb.
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+    */
+
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public User findUserByEmail(@PathParam("email") String email) {
+        try {
+            return ejb.findUserByEmail(email);
+        } catch (FindUserException fue) {
+            throw new InternalServerErrorException(fue.getMessage());
+        }
     }
 
     @GET
-    
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<User> findAll() {
-        return null;
+    public List<User> findUserByName(@PathParam("name") String name) {
+        try {
+            return ejb.findUsersByName(name);
+        } catch (FindUserException fue) {
+            throw new InternalServerErrorException(fue);
+        }
     }
+
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<User> findUserByStatus(@PathParam("status") Status status) {
+        try {
+            return ejb.findUsersByStatus(status);
+        } catch (FindUserException fue) {
+            throw new FindUserException(fue.getMessage());
+        }
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<User> findUserByPrivilege(@PathParam("privilege") Privilege privilege) {
+        try {
+            return ejb.findUsersByPrivilege(privilege);
+        } catch (FindUserException fue) {
+            throw new InternalServerErrorException(fue.getMessage(););
+        }
+    }
+
 
     @GET
     @Path("{from}/{to}")
